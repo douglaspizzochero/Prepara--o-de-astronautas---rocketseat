ca1d837a68d161c627e82eb957ef283b9e44e1b5
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-08-09 16:13:11 -0300

    getElement_querySelector

---
+++ Pilotando com a DOM/script.js
@@ -0,0 +1,33 @@
+// getElementById()
+
+//const element = document.getElementById('blog-title')
+//console.log(element)
+
+//getElementByClassName()
+
+//const element = document.getElementsByClassName("one")
+//console.log(element)
+
+//getElementByTagName()
+//const element = document.getElementsByTagName("meta")
+//const element1 = document.getElementsByTagName("head")
+//posso fazer também com h1, body...
+//console.log(element)
+//console.log(element1)
+
+//querySelector() - pegará o seletor do css
+//ele pegará o primeiro que achar
+
+//const element = document.querySelector(".one")
+//const element1= document.querySelector ("meta")
+//console.log(element)
+//console.log(element1)
+
+//querySelectorAll
+
+const element = document.querySelectorAll ('[src]')
+console.log(element)
+
+
+
+
